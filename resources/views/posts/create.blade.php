9bcc61358206e64cc681e56168d9d770b5b4eedb
ypb-sd12 <takahiro.hado@yoshida-p.co.jp>
2025-05-21 10:11:18 +0900

    Finished Chapter 10

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,56 +5,80 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>新規投稿</title>
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ assert('css/style.css') }}">
 </head>
 
 <body>
-    <header>
-        <nav>
-            <a href="{{ route('posts.index') }}">投稿アプリ</a>
+    <div class="wrapper">
+        <header>
+            <nav class="navbar nabvar-light bg-light">
+                <div class="container">
+                    <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
 
-            <ul>
-                <li>
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-                    <form action="{{ route('logout') }}" id="logout-form" method="post">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    
-    <main>
-        <h1>新規投稿</h1>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                            <form action="{{ route('logout') }}" id="logout-form" method="post">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
 
-        @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
+            </nav>
+        </header>
 
-        <a href="{{ route('posts.index') }}">&lt; 戻る</a>
+        <main>
+            <div class="container">
+                <h1 class="fs-2 my-3">新規投稿</h1>
 
-        <form action="{{ route('posts.store') }}" method="post">
-            @csrf
-            <div>
-                <label for="title">タイトル</label>
-                <!-- <input type="text" id="title" name="title"> -->
-                <input type="text" id="title" name="title" value="{{ old('title') }}">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <div class="mb-2">
+                    <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+                </div>
+
+                <form action="{{ route('posts.store') }}" method="post">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label for="title">タイトル</label>
+                        <!-- <input type="text" id="title" name="title"> -->
+                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="content">本文</label>
+                        <!-- <textarea name="content" id="content"></textarea> -->
+                        <textarea class="form-control" name="content" id="content">{{ old('content') }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-outline-primary">投稿</button>
+                </form>
             </div>
-            <div>
-                <label for="content">本文</label>
-                <!-- <textarea name="content" id="content"></textarea> -->
-                <textarea name="content" id="content">{{ old('content') }}</textarea>
-            </div>
-            <button type="submit">投稿</button>
-        </form>
-    </main>
 
-    <footer>
-        <p>&copy; 投稿アプリ All rights reserved.</p>
-    </footer>
+        </main>
+
+        <footer class="d-flex justify-content-center align-items-center bg-light">
+            <p class="text-muted-small mb-0">&copy; 投稿アプリ All rights reserved.</p>
+        </footer>
+    </div>
+
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,56 +5,80 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>投稿編集</title>
+    <meta http-equiv='refresh' content='4; URL='> <!--画面自動更新-->
+
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ assert('css/style.css') }}">
 </head>
 
 <body>
-    <header>
-        <nav>
-            <a href="{{ route('posts.index') }}">投稿アプリ</a>
+    <div class="wrapper">
+        <header>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container">
+                    <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
 
-            <ul>
-                <li>
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-                    <form action="{{ route('logout') }}" id="logout-form" method="post">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
-        </nav>
-    </header>
+                    <ul class="navbar-nav">
+                        <li class="navbar-item">
+                            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                            <form action="{{ route('logout') }}" id="logout-form" method="post">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
 
-    <main>
-        <h1>投稿編集</h1>
+        <main>
+            <div class="container">
+                <h1 class="fs-2 my-3">投稿編集</h1>
 
-        @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
 
-        <a href="{{ route('posts.index') }}">&lt; 戻る</a>
+                @endif
 
-        <form action="{{ route('posts.update', $post) }}" method="post">
-            @csrf
-            @method('patch')
-            <div>
-                <label for="title">タイトル</label>
-                <input type="text" id="title" name="title" value="{{ old('title', $post->title) }}">
+                <div class="mb-2">
+                    <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+                </div>
+
+                <form action="{{ route('posts.update', $post) }}" method="post">
+                    @csrf
+                    @method('patch')
+                    <div class="form-group mb-3">
+                        <label for="title">タイトル</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="content">本文</label>
+                        <textarea class="form-control" name="content" id="content">{{ old('content', $post->content) }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-outline-primary">更新</button>
+                </form>
             </div>
-            <div>
-                <label for="content">本文</label>
-                <textarea name="content" id="content">{{ old('content', $post->content) }}</textarea>
-            </div>
-            <button type="submit">更新</button>
-        </form>
-    </main>
+        </main>
 
-    <footer>
-        <p>&copy; 投稿アプリ All rights reserved.</p>
-    </footer>
-    
+        <footer class="d-flex justify-content-center align-items-center bg-light">
+            <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
+        </footer>
+    </div>
+
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
